b68151ca41c14caab209eb4f77ccf45fd90db507
YifansCode0506 <yifan.ky0523@outlook.com>
2026-04-22 16:50:38 +0930

    same method but easy to read

--- 2. Sliding Window & Double Pointer/1. Fixed Length/1.1 Fundamentals/5. Minimum Recolors to Get K Consecutive Black Blocks - 2379.js
+++ 2. Sliding Window & Double Pointer/1. Fixed Length/1.1 Fundamentals/5. Minimum Recolors to Get K Consecutive Black Blocks - 2379.js
@@ -4,20 +4,40 @@
  * @return {number}
  */
 var minimumRecolors = function(blocks, k) {
-    let ans = 0, sum = Infinity;
-    
-    for (let r = 0; r < blocks.length; r++){
-        const rightBlock = blocks[r];
-    
-        if (rightBlock === "W") ans++;
+    let len = blocks.length;
+    let cnt_w = 0;
 
-        const l = r - k + 1;
-        if (l < 0 ) continue;
-
-        sum = Math.min(ans, sum);
-        
-        const leftBlock = blocks[l];
-        if (leftBlock === "W") ans--;
+    for (let i = 0; i < k; i++) {
+        cnt_w += (blocks[i] === 'W' ? 1 : 0);
     }
-    return sum;
+
+    let res = cnt_w;
+
+    for (let right = k; right < len; right++) {
+        let _in = blocks[right];
+        let _out = blocks[right - k]
+        cnt_w = cnt_w + (_in === 'W' ? 1 : 0) - (_out === 'W' ? 1 : 0)
+        res = Math.min(res, cnt_w)
+    }
+    return res
 };
+
+
+// var minimumRecolors = function(blocks, k) {
+//     let ans = 0, sum = Infinity;
+    
+//     for (let r = 0; r < blocks.length; r++){
+//         const rightBlock = blocks[r];
+    
+//         if (rightBlock === "W") ans++;
+
+//         const l = r - k + 1;
+//         if (l < 0 ) continue;
+
+//         sum = Math.min(ans, sum);
+        
+//         const leftBlock = blocks[l];
+//         if (leftBlock === "W") ans--;
+//     }
+//     return sum;
+// };
