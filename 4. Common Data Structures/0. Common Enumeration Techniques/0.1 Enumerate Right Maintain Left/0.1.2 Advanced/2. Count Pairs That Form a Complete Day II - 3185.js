2c935d6a54614bf6d9268c46fa1b1ebd3dde8761
YifansCode0506 <yifan.ky0523@outlook.com>
2026-07-09 16:17:53 +0930

    3185

--- 4. Common Data Structures/0. Common Enumeration Techniques/0.1 Enumerate Right Maintain Left/0.1.2 Advanced/2. Count Pairs That Form a Complete Day II - 3185.js
+++ 4. Common Data Structures/0. Common Enumeration Techniques/0.1 Enumerate Right Maintain Left/0.1.2 Advanced/2. Count Pairs That Form a Complete Day II - 3185.js
@@ -29,3 +29,17 @@ var countCompleteDayPairs = function(hours) {
     // 返回累计出的总配对对数
     return ans;
 };
+
+var countCompleteDayPairs = function(hours) {
+    const H = 24;
+    const cnt = Array(H).fill(0);
+    let ans = 0;
+
+    for (const t of hours) {
+        // 先查询 cnt，再更新 cnt，因为题目要求 i < j
+        // 如果先更新，再查询，就把 i = j 的情况也考虑进去了
+        ans += cnt[(H - t % H) % H];
+        cnt[t % H]++;
+    }
+    return ans;
+}; 
